2cdc269853acf8b20b78e4b7acfd4c7d69b38636
Camila Huchin <camila.h.huchin@ciencias.unam.mx>
2025-08-26 19:52:19 -0600

    Implementación práctica p2_api-product

---
+++ Clases/Api/costumer/src/main/java/com/costumer/api/customer/entity/Region.java
@@ -0,0 +1,9 @@
+package com.costumer.api.customer.entity;
+
+public class Region {
+    private Integer region_id;
+    private String region;
+    private String tag;
+    private Integer status;
+}
+
