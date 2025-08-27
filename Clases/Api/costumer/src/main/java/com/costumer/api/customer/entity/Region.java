b96219806898ebd7d21f4db37354c923cb0b56ab
Camila Huchin <camila.h.huchin@ciencias.unam.mx>
2025-08-26 20:47:23 -0600

    Eliminación de carpeta equivocada

--- Clases/Api/costumer/src/main/java/com/costumer/api/customer/entity/Region.java
+++
@@ -1,9 +0,0 @@
-package com.costumer.api.customer.entity;
-
-public class Region {
-    private Integer region_id;
-    private String region;
-    private String tag;
-    private Integer status;
-}
-
